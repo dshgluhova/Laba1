656a6df5225e93b9c328294b8ee689fa3a3651e4
Serpash <ser7290@gmail.com>
2021-02-18 20:30:12 +0300

    Added: Main

--- src/bsuir/group922402/var6/gluhova/Main.java
+++ src/bsuir/group922402/var6/gluhova/Main.java
@@ -1,5 +1,11 @@
-package bsuir.group922402.var6.gluhova;
+package bsuir.group922402.laba1.var6.gluhova;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+
+//Заданы два массива – А(5) и В(4). Первым на печать вывести массив, содержащий наибольшее значение. Напечатать также это значение и его порядковый номер.
 
 public class Main {
-
-}
+    public static void main(String args[])
